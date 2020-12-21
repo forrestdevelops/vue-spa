a5f0e7e08cf172d0b26a11d579c81ff70845ae2e
Forrest Smietanski <fsmietanski@gmail.com>
2020-12-21 09:53:07 -0500

    initial build out of application structure

---
+++ src/components/header-bar.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <nav
+      class="navbar has-background-dark is-dark"
+      role="navidation"
+      aria-label="main navigation"
+      >
+      <HeaderBarBrand></HeaderBarBrand>
+      <HeaderBarLinks></HeaderBarLinks>
+    </nav>
+  </header>
+</template>
+
+<script>
+import HeaderBarBrand from '@/components/header-bar-brand';
+import HeaderBarLinks from '@/components/header-bar-links';
+
+export default {
+  name: 'HeaderBar',
+  comments: {HeaderBarBrand, HeaderBarLinks},
+}
+</script>
